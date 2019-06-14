5af4116e6939ab3be2b189e6195fd8457f216e55
Phillip Dexheimer <phillip.dexheimer@cchmc.org>
2019-06-14 11:17:00 -0400

    Allow for adding multiple annotations from a single file

--- modules/annotation.py
+++ modules/annotation.py
@@ -1,3 +1,19 @@
+# scuttle - manage and manipulate sc-rna data files
+# Copyright (C) 2019 Phillip Dexheimer
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 Manage cell/gene annotations - add from external sources, etc
 """
@@ -11,10 +27,24 @@ def add_arguments(arg_parser):
     ann_group.add_argument('--cell-annot-no-header', action='store_true', help='If specified, the file used in --cell-annot-file should not have a header line')
     ann_group.add_argument('--cell-annotation', metavar='NAME', help='The name of the cell annotation to add')
     ann_group.add_argument('--cell-id-column', metavar='COLUMN', type=int, help='The (0-based) column number in cell_annot_file that contains the cell ID', default=0)
-    ann_group.add_argument('--cell-annot-column', metavar='COLUMN', type=int, help='The (0-based) column number in cell_annot_file that contains the annotation data to be added', default=1)
+    ann_group.add_argument('--cell-annot-column', metavar='COLUMN', help='The (0-based) column number in cell_annot_file that contains the annotation data to be added', default='1')
     ann_group.add_argument('--cell-suffix', metavar='SUFFIX', help='A suffix added to every cell id in cell_annot_file to match the data (ie, "-1")', default='-1')
 
     ann_group.add_argument('--drop-cell-annot', metavar='NAME', action='append', help='PERMANENTLY remove the indicated cell annotation')
+
+def validate_args(args):
+    cell_annotations = ( args.cell_annot_file is not None,
+                         args.cell_annotation is not None,
+                         args.cell_id_column is not None,
+                         args.cell_annot_column is not None)
+    if any(cell_annotations) and not all(cell_annotations):
+        logging.critical("Cell annotations have only been partially specified (see --cell-annot-file, --cell-annotation, --cell-id-column, and --cell-annot-column)")
+        exit(1)
+    args.cell_annotation = args.cell_annotation.split(',')
+    args.cell_annot_column = [ int(x) for x in args.cell_annot_column.split(',') ]
+    if len(args.cell_annotation) != len(args.cell_annot_column):
+        logging.critical("Length of --cell-annotation must match --cell-annot-column")
+        exit(1)
 
 def process(data, args):
     if args.drop_cell_annot:
@@ -28,7 +58,8 @@ def add_cell_annotation(data, filename, header_absent, annot_name, cell_column, 
     annot = pd.read_csv(filename, sep='\t', header=header_row, index_col=cell_column)
     if cell_suffix:
         annot.rename(lambda x: x+cell_suffix, inplace=True)
-    data.obs[annot_name] = annot[annotation_column]
+    for name, col in zip(annot_name, annotation_column):
+        data.obs[name] = annot[col]
 
 def drop_cell_annotation(data, annotations):
     for annot_name in annotations:
--- singlecell.py
+++ singlecell.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+# scuttle - manage and manipulate sc-rna data files
+# Copyright (C) 2019 Phillip Dexheimer
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from argparse import ArgumentParser
 from modules import annotation, cellecta, file, summarize, filter
 import logging.config
@@ -35,6 +51,7 @@ def parse_arguments():
 
 def validate_arguments(args):
     file.validate_args(args)
+    annotation.validate_args(args)
 
 def process_data(data, args):
     annotation.process(data, args)
